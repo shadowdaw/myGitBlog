586225386af9307396693f4ab82a7dd265023c73
于洲 <shadowdaw@vip.qq.com>
2017-10-12 00:05:04 +0800

    新增文章“你好，少年”

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var category = req.query.category;
-    res.render('index', { title: 'Insomnia-er', index: index });
+    var tag = req.query.tag;
+    var blogIndex = {};
+    if (tag) {
+        for (let k in index) {
+            var blog = index[k];
+            if (blog.tag == tag) {
+                blogIndex[k] = blog;
+            }
+        }
+    } else {
+        blogIndex = index;
+    }
+    res.render('index', { title: 'Insomnia-er', index: blogIndex });
 });
 
+/* GET users listing. */
+router.get('/update', function(req, res, next) {
+    var indexPath = path.join(__dirname, '../blogIndex.json')
+    fs.readFile(indexPath, function(err, data) {
+        global.index = JSON.parse(data.toString());
+        res.redirect('/');
+    })
+});
 module.exports = router;
